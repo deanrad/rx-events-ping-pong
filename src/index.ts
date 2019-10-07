5b7ed12811a276c2229d1f27f663b612aa905e80
Dean Radcliffe <deanius@users.noreply.github.com>
2019-10-07 16:29:33 -0500

    0.1 Event handlers with Promises

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
-import { on, trigger, after, spy } from "rx-helper";
+import { on, trigger } from "rx-helper";
 
 const ballElem = document.getElementById("ball");
 
 on("ping", () => {
   ballElem.style.left = "95vw";
-  return after(2000, () => {
+  new Promise(resolve => setTimeout(resolve, 2000)).then(() => {
     trigger("pong");
   });
 });
 
 on("pong", () => {
   ballElem.style.left = "1vw";
-  return after(2000, () => {
+  new Promise(resolve => setTimeout(resolve, 2000)).then(() => {
     trigger("ping");
   });
 });
 
-spy(({ type }) => console.log(`${type}`));
 trigger("ping");
