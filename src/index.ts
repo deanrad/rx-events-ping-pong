1da1857cd1bfbd02704a224de5118603220ba021
Dean Radcliffe <deanius@users.noreply.github.com>
2020-07-13 11:37:49 -0500

    fancified

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import { listen, trigger, after, reset } from "polyrhythm";
-
 const ballElem = document.getElementById("ball");
 
 // HMR-friendliness
 reset();
 console.clear();
 
-listen("ping", () => {
-  ballElem.style.left = "95vw";
-  ballElem.style.transform = "rotate(100deg)";
+const player1 = listen("ping", () => {
+  ballElem.style.left = "90vw";
+  ballElem.style.transform = "rotate(-320deg)";
   // Important: 'After' will not start unless we await it!
   return after(2000, () => {
     trigger("pong");
@@ -25,3 +24,7 @@ listen("pong", () => {
 
 listen(true, ({ type }) => console.log(`${type}`));
 trigger("ping");
+
+after(12000, () => {
+  player1.unsubscribe();
+}).then(() => console.log("Player 1 is Going Home!"));
